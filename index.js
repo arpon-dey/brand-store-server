2de9864e4ca47b08301ab4b2cb1134d722eb2ca6
arpon-dey <arpondeydurjoy@gmail.com>
2023-10-20 21:59:58 +0600

    user can delete data now

--- index.js
+++ index.js
@@ -95,6 +95,14 @@ async function run() {
     })
 
 
+    app.delete('/myCart/:id', async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) }
+        const result = await cartCollection.deleteOne(query);
+        res.send(result);
+    })
+
+
 
 
     await client.db("admin").command({ ping: 1 });
